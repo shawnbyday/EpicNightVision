2f6e5bb2830f19bf550b6e9971dab83c6ef93cbe
shawnbyday <social@shawnbyday.com>
2022-12-12 17:19:19 -0600

    Have working night vision armor piece. Needs texturing, recipe work, and JEI integration.

--- src/main/java/com/shawnbyday/epicnightvision/item/ModItems.java
+++ src/main/java/com/shawnbyday/epicnightvision/item/ModItems.java
@@ -1,6 +1,8 @@
 package com.shawnbyday.epicnightvision.item;
 
 import com.shawnbyday.epicnightvision.EpicNightVision;
+import com.shawnbyday.epicnightvision.item.custom.ModArmorItem;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -10,7 +12,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, EpicNightVision.MOD_ID);
 
-    public static final RegistryObject<Item> GOGGLES = ITEMS.register("goggles", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.ENV_TAB)));
+    public static final RegistryObject<Item> GOGGLES = ITEMS.register("goggles", () -> new ModArmorItem(ModArmorMaterials.GOGGLES, EquipmentSlot.HEAD, new Item.Properties().tab(ModCreativeModeTab.ENV_TAB)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
---
+++ src/main/java/com/shawnbyday/epicnightvision/item/custom/ModArmorItem.java
@@ -0,0 +1,69 @@
+package com.shawnbyday.epicnightvision.item.custom;
+
+import com.google.common.collect.ImmutableMap;
+import com.shawnbyday.epicnightvision.item.ModArmorMaterials;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+import java.util.Map;
+
+public class ModArmorItem extends ArmorItem {
+    private static final Map<ArmorMaterial, MobEffectInstance> MATTERIAL_TO_EFFECT_MAP = (new ImmutableMap.Builder<ArmorMaterial, MobEffectInstance>()).put(ModArmorMaterials.GOGGLES, new MobEffectInstance(MobEffects.NIGHT_VISION, 400, 1)).build();
+
+    public ModArmorItem(ArmorMaterial material, EquipmentSlot slot, Properties settings) {
+        super(material, slot, settings);
+    }
+    @Override
+    public void onArmorTick(ItemStack stack, Level world, Player player) {
+        if(!world.isClientSide()) {
+            if(hasGogglesOn(player)) {
+                evaluateArmorEffects(player);
+            }
+        }
+    }
+
+    private void evaluateArmorEffects(Player player) {
+        for(Map.Entry<ArmorMaterial, MobEffectInstance> entry : MATTERIAL_TO_EFFECT_MAP.entrySet()) {
+            ArmorMaterial mapArmorMaterial = entry.getKey();
+            MobEffectInstance mapStatusEffect = entry.getValue();
+
+            if(hasCorrectGogglesOn(mapArmorMaterial, player)) {
+                addStatusEffectForMaterial(player, mapArmorMaterial, mapStatusEffect);
+            }
+        }
+    }
+
+    private void addStatusEffectForMaterial(Player player, ArmorMaterial mapArmorMaterial, MobEffectInstance mapStatusEffect) {
+        boolean hasPlayerEffect = player.hasEffect(mapStatusEffect.getEffect());
+
+        if(hasCorrectGogglesOn(mapArmorMaterial, player) && !hasPlayerEffect) {
+            player.addEffect(new MobEffectInstance(mapStatusEffect.getEffect(), mapStatusEffect.getDuration(), mapStatusEffect.getAmplifier()));
+        } else {
+            if(player.hasEffect(MobEffects.NIGHT_VISION)) {
+                int duration = player.getEffect(MobEffects.NIGHT_VISION).getDuration();
+                if (duration < 360) {
+                    player.addEffect(new MobEffectInstance(mapStatusEffect.getEffect(), mapStatusEffect.getDuration(), mapStatusEffect.getAmplifier()));
+                }
+            }
+        }
+    }
+
+    private boolean hasGogglesOn(Player player) {
+        ItemStack goggles = player.getInventory().getArmor(3);
+
+        return !goggles.isEmpty();
+    }
+
+    private boolean hasCorrectGogglesOn(ArmorMaterial material, Player player) {
+        ArmorItem goggles = ((ArmorItem)player.getInventory().getArmor(3).getItem());
+
+        return goggles.getMaterial() == material;
+    }
+}
